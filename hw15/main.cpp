d55eee1c527342696f448bc67870a0d385cf4b77
Jeremy Wright <jeremy@codestrokes.com>
2013-11-24 11:28:04 -0700

    Plotting 2 lines. Need to clean up a buit.

--- hw15/main.cpp
+++ hw15/main.cpp
@@ -150,61 +150,20 @@ double p(lime_type, data_type)
 
 using dataset_t = std::vector<std::pair<double, double>>;
 
-void plot(dataset_t dataset, string filename)
+void plot(dataset_t dataset1, dataset_t dataset2, string filename)
 {
     Gnuplot gp;
-    
-    
-   	std::vector<std::pair<double, double> > xy_pts_B;
-	for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
-		double theta = alpha*2.0*3.14159;
-		xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
-	}
-
-	std::cout << "Creating my_graph_2.png" << std::endl;
+	
 	gp << "set output '"<< filename << ".png'\n";
-	gp << "set xrange [-2:12]\nset yrange [0:1]\n";
-    gp << "plot '-' with lines title '"<< filename << "'\n";
+	gp << "set xrange [0:12]\nset yrange [0:1]\n";
+    gp << "plot '-' with lines title '"<< filename << "'";
 
-    gp.send1d(dataset);
+    gp << ",'-' with lines title '"<< filename << "2'\n";
+    gp.send1d(dataset1);
+
+    gp.send1d(dataset2);
 }
 
-void demo_png() {
-	Gnuplot gp;
-
-	gp << "set terminal png\n";
-
-	std::vector<double> y_pts;
-	for(int i=0; i<1000; i++) {
-		double y = (i/500.0-1) * (i/500.0-1);
-		y_pts.push_back(y);
-	}
-
-	std::cout << "Creating my_graph_1.png" << std::endl;
-	gp << "set output 'my_graph_1.png'\n";
-	gp << "plot '-' with lines, sin(x/200) with lines\n";
-	gp.send1d(y_pts);
-
-	std::vector<std::pair<double, double> > xy_pts_A;
-	for(double x=-2; x<2; x+=0.01) {
-		double y = x*x*x;
-		xy_pts_A.push_back(std::make_pair(x, y));
-	}
-
-	std::vector<std::pair<double, double> > xy_pts_B;
-	for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
-		double theta = alpha*2.0*3.14159;
-		xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
-	}
-
-	std::cout << "Creating my_graph_2.png" << std::endl;
-	gp << "set output 'my_graph_2.png'\n";
-	gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
-    gp << "plot '-' with lines title 'cubic', '-' with points title 'circle'\n";
-
-    gp.send1d(xy_pts_A);
-    gp.send1d(xy_pts_B);
-}
 
 int main(int argc, char* argv[])
 {
@@ -284,11 +243,11 @@ int main(int argc, char* argv[])
         //    cout << p.first << " : " << p.second << endl;
         //}
         //cout << endl;
-        plot(h1, string("H1_Line_")+std::to_string(lineno));
-        plot(h2, string("H2_Line_")+std::to_string(lineno));
-        plot(h3, string("H3_Line_")+std::to_string(lineno));
-        plot(h4, string("H4_Line_")+std::to_string(lineno));
-        plot(h5, string("H5_Line_")+std::to_string(lineno));
+        //plot(h1, string("H1_Line_")+std::to_string(lineno));
+        plot(h2, h3, string("H2_Line_")+std::to_string(lineno));
+        //plot(h3, string("H3_Line_")+std::to_string(lineno));
+        //plot(h4, string("H4_Line_")+std::to_string(lineno));
+        //plot(h5, string("H5_Line_")+std::to_string(lineno));
         cout << endl;
 
     }
